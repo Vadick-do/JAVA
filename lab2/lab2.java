74d95b90ee007f07df2855002c7890fd57d7d0ec
Vadick-do <vadimkaptel266@gmail.com>
2026-09-21 09:40:44 +0300

    base lab2

--- lab2/lab2.java
+++ lab2/lab2.java
@@ -1,5 +1,9 @@
 package lab2;
 
 public class lab2 {
-    
+
+    public static void main(String[] args) {
+        WebCamera defaultCamera = new WebCamera();
+        System.out.println(defaultCamera);
+    }
 }
