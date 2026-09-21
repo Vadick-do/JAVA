56f6b79852e7b366653c72c15e355644fcc576b3
Vadick-do <vadimkaptel266@gmail.com>
2026-09-21 17:19:44 +0300

    switch menu

--- lab2/lab2.java
+++ lab2/lab2.java
@@ -1,14 +1,57 @@
 package lab2;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class lab2 {
 
     public static void main(String[] args) {
-        WebCamera defaultCamera = new WebCamera();
-        WebCamera cam = new WebCamera("Logitech C270 / C310", 640, 90);
-        WebCamera[] cams = new WebCamera[]{defaultCamera, cam};
-        WebCamera bestCam = getBestCamera(cams);
-        String model = bestCam.getModel();
-        System.out.println(model);
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<WebCamera> cams = new ArrayList<>();
+        
+        cams.add(new WebCamera());
+        cams.add(new WebCamera("Logitech C270 / C310", 640, 90));
+       
+        boolean isRunning = true;
+        while (isRunning) {
+            System.out.println("\n--- МЕНЮ ---");
+            System.out.println("0. Выход");
+            System.out.println("1. Просмотр всех веб-камер");
+            System.out.println("2. Добавить веб-камеру");
+            System.out.println("3. Изменить свойства камеры по номеру");
+            System.out.println("4. Удалить камеру по номеру");
+            System.out.println("5. Найти камеру с наилучшими характеристиками");
+            System.out.print("Выберите действие: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    //printCameras(cams);
+                    break;
+                case 2:
+                    //addCamera(cams, scanner);
+                    break;
+                case 3:
+                    //editCamera(cams, scanner);
+                    break;
+                case 4:
+                    //deleteCamera(cams, scanner);
+                    break;
+                case 5:
+                    //showBestCamera(cams);
+                    break;
+                case 0:
+                    System.out.println("Выход из программы.");
+                    isRunning = false;
+                    break;
+                default:
+                    System.out.println("Такого в меню нет!");
+            }
+        }
+    
+        scanner.close();
     }
 
     public static WebCamera getBestCamera(WebCamera[] cams) {
